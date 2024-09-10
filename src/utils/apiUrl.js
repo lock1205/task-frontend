ec56e07ce7bd0d3447569d83a4ec3399d2673017
lock1205 <mindheal1205@gmail.com>
2024-09-10 17:38:39 +0900

    first prod test commit

---
+++ src/utils/apiUrl.js
@@ -0,0 +1,14 @@
+const GET_TASKS_API_URL = 'http://a.gorideadpoets.com/get_tasks';
+const POST_TASKS_API_URL = 'http://a.gorideadpoets.com/post_task';
+const DELETE_TASKS_API_URL = 'http://a.gorideadpoets.com/delete_task';
+const UPDATE_COMPLETED_TASKS_API_URL =
+  'http://a.gorideadpoets.com/update_completed_task';
+const UPDATE_TASK_API_URL = 'http://a.gorideadpoets.com/update_task';
+
+export {
+  GET_TASKS_API_URL,
+  POST_TASKS_API_URL,
+  DELETE_TASKS_API_URL,
+  UPDATE_COMPLETED_TASKS_API_URL,
+  UPDATE_TASK_API_URL,
+};
